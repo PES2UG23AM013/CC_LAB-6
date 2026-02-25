dc4b60ac9b5f90a0b746134bd4b0be00ae70b03a
PES2UG23AM013 <PES2UG23AM013@pesu.pes.edu>
2026-02-25 23:55:30 +0530

    Fixed app.cpp with proper main function

---
+++ backend/app.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    while (true) {
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+        std::string response = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nServed by backend\n";
+
+        send(new_socket, response.c_str(), response.length(), 0);
+        close(new_socket);
+    }
+
+    return 0;
+}
